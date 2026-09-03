603bb1e008cd2c04e26b16861adedd3d02b4b2ea
Jakub Filip <jakubfilip@innovativesoftwarecenter.dev>
2026-09-04 00:34:15 +0200

    NullCache.php

---
+++ modules/gateways/tpay/Cache/NullCache.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace JakubFilip\Tpay\Cache;
+
+use Psr\SimpleCache\CacheInterface;
+use DateInterval;
+
+class NullCache implements CacheInterface
+{
+
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return $default;
+    }
+
+    public function set(string $key, mixed $value, DateInterval|int|null $ttl = null): bool
+    {
+        return true;
+    }
+
+    public function delete(string $key): bool
+    {
+        return true;
+    }
+
+    public function clear(): bool
+    {
+        return true;
+    }
+
+    public function getMultiple(iterable $keys, mixed $default = null): iterable
+    {
+        $result = [];
+
+        foreach ($keys as $key) {
+            $result[$key] = $default;
+        }
+
+        return $result;
+    }
+
+    public function setMultiple(iterable $values, DateInterval|int|null $ttl = null): bool
+    {
+        return true;
+    }
+
+    public function deleteMultiple(iterable $keys): bool
+    {
+        return true;
+    }
+
+    public function has(string $key): bool
+    {
+        return false;
+    }
+}
